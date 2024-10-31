6214d9c4f280b5e1312113b8a08a654a3159ab8a
P1tt1cus <30161177+P1tt1cus@users.noreply.github.com>
2024-10-31 22:58:42 +1100

    Amend repo error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://yourusername.github.io',
-  base: '/your-repo-name',
+  site: 'https://p1tt1cus.github.io',
+  base: '/bloggers',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
       theme: 'tokyo-night',
-      wrap: true
-    }
-  }
+      wrap: true,
+    },
+  },
 });
